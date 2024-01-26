7055074cbc841b70fdd25ecb5c96d99ddc616459
Zwidar <50343618+Zwidar@users.noreply.github.com>
2024-01-26 18:47:34 -0500

    only display nth term
    changed code to be more like what assignment wanted. only display the nth term.

--- fibseq/src/fibseq/fibseq.java
+++ fibseq/src/fibseq/fibseq.java
@@ -8,7 +8,7 @@ public class fibseq {
 	static int chosenNbr = 0;
 	static int firstNbr = 0;
 	static int secondNbr = 1;
-
+	static int newNbr = 0;
 	/**
 	 * This starts the app and gets the user desired number of output.
 	 * @param args default unused.
@@ -20,8 +20,9 @@ public class fibseq {
         chosenNbr = scanner.nextInt();
     	
     	
-	    System.out.print("Fib Numbers: 0 1 ");
-	    fibCalc(firstNbr, secondNbr, 2, chosenNbr);
+
+	    fibCalc(firstNbr, secondNbr, 1, chosenNbr);
+		System.out.print("The " + chosenNbr + "th term of Fibonacci is " + newNbr);
 		System.out.println();
         scanner.close();
     }
@@ -29,18 +30,21 @@ public class fibseq {
 	/**
 	 * This function calculates numbers in the Fibonacci sequence.
 	 * Outputs next number in the sequence up to the total number chosen by the user.
-	 * @param firstNbr First number for addition to calculate new Fibonnaci Number.
+	 *
+	 * @param firstNbr  First number for addition to calculate new Fibonnaci Number.
 	 * @param secondNbr Second number for addition to calculate new Fibonnaci Number.
-	 * @param countNbr Counts the number of iterations it's cycling through.
+	 * @param countNbr  Counts the number of iterations it's cycling through.
 	 * @param chosenNbr The number provided by the user for desired number of output.
+	 * @return
 	 */
-	private static void fibCalc(int firstNbr, int secondNbr, int countNbr, int chosenNbr) {
+	private static int fibCalc(int firstNbr, int secondNbr, int countNbr, int chosenNbr) {
 		if (countNbr < chosenNbr) {
-			int newNbr = firstNbr + secondNbr;
+			newNbr = firstNbr + secondNbr;
          	countNbr = countNbr + 1;
-			System.out.print(newNbr + " ");
+			//System.out.print(newNbr + " ");
 			fibCalc(secondNbr, newNbr, countNbr, chosenNbr);
 		}
-	} 
+		return newNbr;
+	}
 }
 
